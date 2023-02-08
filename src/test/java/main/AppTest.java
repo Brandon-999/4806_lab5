8cab9dace43610b75fd8666c9b9f448ed3c61a02
b <b>
2023-02-08 17:21:17 -0500

    tests working

--- src/test/java/main/AppTest.java
+++ src/test/java/main/AppTest.java
@@ -1,20 +1,41 @@
 package main;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.web.client.RestTemplate;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AppTest {
+
+	@Autowired
+	private TestController controller;
+
+	@Test
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
+	}
+
+    @Value(value="${local.server.port}")
+	private int port;
+
+	//@Autowired
+	private RestTemplate restTemplate = new RestTemplate();
+
+	@Test
+	public void greetingShouldReturnDefaultMessage() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+				String.class)).contains("Hello Spring Boot App");
+	}
+
+	@org.junit.Test
+	public void tsetThymleafExample() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/tst",
+				String.class)).contains("Current time");
+	}
 }
